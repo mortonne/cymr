883d7ad713062e022fed9fd1b50ba9b43fc98b21
Neal Morton <mortonne@gmail.com>
2020-04-12 13:34:27 -0500

    build: setup file with cython support

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='cymr',
+    version='0.1.0',
+    description='CMR implemented using Cython.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Neal Morton',
+    author_email='mortonne@gmail.com',
+    license='GPLv3',
+    url='http://github.com/mortonne/cymr',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    ext_modules=cythonize('src/cymr/operations.pyx'),
+    install_requires=[
+        'numpy',
+        'cython',
+        'pytest'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
