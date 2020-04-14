ac96a2e3a754ab5462920a854f9e29c1735e2698
Neal Morton <mortonne@gmail.com>
2020-04-14 03:01:10 -0500

    perf: add cython function for present

--- src/cymr/operations.pyx
+++ src/cymr/operations.pyx
@@ -22,3 +22,46 @@ def update(double [:] c, double[:] c_in, double B):
 
     for i in range(n):
         c[i] = rho * c[i] + B * c_in[i]
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def present(const double [:, :] w_fc_exp,
+            const double [:, :] w_fc_pre,
+            double [:] c,
+            double [:] c_in,
+            double [:] f,
+            int item,
+            double B):
+    cdef Py_ssize_t n_f = f.shape[0]
+    cdef Py_ssize_t n_c = c.shape[0]
+    cdef int i
+
+    # set item unit
+    for i in range(n_f):
+        if i == item:
+            f[i] = 1
+        else:
+            f[i] = 0
+
+    # set c_in
+    for i in range(n_c):
+        c_in[i] = w_fc_exp[item, i] + w_fc_pre[item, i]
+
+    # get the vector length of the input to context
+    cdef double sum_squares = 0
+    for i in range(n_c):
+        sum_squares += c_in[i] * c_in[i]
+    cdef double norm = sqrt(sum_squares)
+    for i in range(n_c):
+        c_in[i] /= norm
+
+    # calculate scaling factor
+    cdef double cdot = 0
+    for i in range(n_c):
+        cdot += c[i] * c_in[i]
+    rho = calc_rho(cdot, B)
+
+    # integrate context
+    for i in range(n_c):
+        c[i] = rho * c[i] + B * c_in[i]
