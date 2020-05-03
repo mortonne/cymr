674678338e703e0cfe280aae3e3279b16efb6e90
Neal Morton <mortonne@gmail.com>
2020-05-03 00:10:26 -0500

    test: initialization of distributed cmr

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from cymr import models
+from cymr import network
 
 
 @pytest.fixture()
@@ -50,3 +51,31 @@ def test_cmr_fit(data):
     results = model.fit_indiv(data, fixed, var_names, var_bounds, n_jobs=2)
     np.testing.assert_allclose(results['B_enc'].to_numpy(),
                                np.array([0.72728744, 0.99883425]), atol=0.01)
+
+
+@pytest.fixture()
+def patterns():
+    cat = np.array([[1, 0, 1, 1, 0, 1],
+                    [0, 1, 0, 0, 1, 0]]).T
+    patterns = {'vector': {'loc': np.eye(6), 'cat': cat},
+                'similarity': {'loc': np.eye(6), 'cat': np.dot(cat, cat.T)}}
+    return patterns
+
+
+def test_init_dist_cmr(patterns):
+    weights_template = {'fcf': {'loc': 'w_loc', 'cat': 'w_cat'},
+                        'ff': {'loc': 'w_loc', 'cat': 'w_cat'}}
+    params = {'w_loc': 1, 'w_cat': 2}
+    weights = network.unpack_weights(weights_template, params)
+    scaled = network.prepare_patterns(patterns, weights)
+    item_index = np.arange(3)
+    net = models.init_dist_cmr(item_index, scaled)
+    expected = np.array([[0.5774, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+                          0.8165, 0.0000, 0.0000],
+                         [0.0000, 0.5774, 0.0000, 0.0000, 0.0000, 0.0000,
+                          0.0000, 0.8165, 0.0000],
+                         [0.0000, 0.0000, 0.5774, 0.0000, 0.0000, 0.0000,
+                          0.8165, 0.0000, 0.0000],
+                         [0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+                          0.0000, 0.0000, 1.0000]])
+    np.testing.assert_allclose(net.w_fc_pre, expected, atol=0.0001)
