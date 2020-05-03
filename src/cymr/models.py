d3db6796e8eaf1fe2af58e9e2c9b7be7601862b7
Neal Morton <mortonne@gmail.com>
2020-05-03 03:02:47 -0500

    refactor: split off CMRDistributed as a separate class

--- src/cymr/models.py
+++ src/cymr/models.py
@@ -179,3 +179,54 @@ class CMR(Recall):
             state.extend(rec)
             net_state.append(state)
         return net_state
+
+
+class CMRDistributed(Recall):
+
+    def prepare_sim(self, data):
+        study, recall = prepare_lists(data, study_keys=['input', 'item_index'],
+                                      recall_keys=['input'], clean=True)
+        return study, recall
+
+    def likelihood_subject(self, study, recall, param, patterns=None,
+                           weights=None):
+        n_item = len(study['input'][0])
+        n_list = len(study['input'])
+        p_stop = network.p_stop_op(n_item, param['X1'], param['X2'])
+        Lfc = np.tile(param['Lfc'], n_item).astype(float)
+        Lcf = network.primacy(n_item, param['Lcf'], param['P1'], param['P2'])
+
+        weights_param = network.unpack_weights(weights, param)
+        scaled = network.prepare_patterns(patterns, weights_param)
+        logl = 0
+        for i in range(n_list):
+            net = init_dist_cmr(study['item_index'][i], scaled)
+            net.study('item', study['input'][i], param['B_enc'], Lfc, Lcf)
+            p = net.p_recall('item', recall['input'][i], param['B_rec'],
+                             param['T'], p_stop)
+            if np.any(np.isnan(p)) or np.any((p <= 0) | (p >= 1)):
+                logl = -10e6
+                break
+            logl += np.sum(np.log(p))
+        return logl
+
+    def generate_subject(self, study_data, param, patterns=None, weights=None,
+                         **kwargs):
+        study = prepare_study(study_data, study_keys=['position', 'item_index'])
+        n_item = len(study['position'][0])
+        n_list = len(study['position'])
+        p_stop = network.p_stop_op(n_item, param['X1'], param['X2'])
+        Lfc = np.tile(param['Lfc'], n_item).astype(float)
+        Lcf = network.primacy(n_item, param['Lcf'], param['P1'], param['P2'])
+
+        weights_param = network.unpack_weights(weights, param)
+        scaled = network.prepare_patterns(patterns, weights_param)
+        recalls = []
+        for i in range(n_list):
+            net = init_dist_cmr(study['item_index'][i], scaled)
+            net.study('item', study['position'][i], param['B_enc'], Lfc, Lcf)
+            recall = net.generate_recall('item', param['B_rec'], param['T'],
+                                         p_stop)
+            recalls.append(recall)
+        data = fit.add_recalls(study_data, recalls)
+        return data
