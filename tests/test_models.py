ef1f44a2965fca28ce0ebb1131edf3a3a2cf25ba
Neal Morton <mortonne@gmail.com>
2020-05-08 21:15:18 -0500

    test: set B_start parameter

--- tests/test_models.py
+++ tests/test_models.py
@@ -112,7 +112,7 @@ def test_dist_cmr(data):
     """Test localist CMR using the distributed framework."""
     patterns = {'vector': {'loc': np.eye(6)}}
     weights_template = {'fcf': {'loc': 'w_loc'}}
-    param = {'B_enc': .5, 'B_rec': .8, 'w_loc': 1,
+    param = {'B_enc': .5, 'B_start': 0, 'B_rec': .8, 'w_loc': 1,
              'Afc': 0, 'Dfc': 1, 'Acf': 1, 'Dcf': 1, 'Aff': 0, 'Dff': 1,
              'Lfc': 1, 'Lcf': 1, 'P1': 0, 'P2': 1,
              'T': 10, 'X1': .05, 'X2': 1}
@@ -127,7 +127,7 @@ def test_dist_cmr_fit(data):
     model = models.CMRDistributed()
     patterns = {'vector': {'loc': np.eye(6)}}
     weights_template = {'fcf': {'loc': 'w_loc'}}
-    fixed = {'B_rec': .8, 'w_loc': 1,
+    fixed = {'B_start': 0, 'B_rec': .8, 'w_loc': 1,
              'Afc': 0, 'Dfc': 1, 'Acf': 0, 'Dcf': 1, 'Aff': 0, 'Dff': 1,
              'Lfc': 1, 'Lcf': 1, 'P1': 0, 'P2': 1,
              'T': 10, 'X1': .05, 'X2': 1}
