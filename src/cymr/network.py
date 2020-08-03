9537db0c122c9798e4bb7eb8a8e5b022c99e7d20
Neal Morton <mortonne@gmail.com>
2020-08-02 19:58:34 -0500

    feat: method to get a segment of a layer

--- src/cymr/network.py
+++ src/cymr/network.py
@@ -330,6 +330,15 @@ class Network(object):
         c_ind = self.c_ind.get_segment(*c_segment)
         return f_ind, c_ind
 
+    def get_segment(self, layer, sublayer, segment):
+        if layer == 'f':
+            ind = self.f_ind.get_segment(sublayer, segment)
+        elif layer == 'c':
+            ind = self.c_ind.get_segment(sublayer, segment)
+        else:
+            raise ValueError(f'Invalid layer: {layer}')
+        return ind
+
     def get_unit(self, layer, sublayer, segment, unit):
         if layer == 'f':
             ind = self.f_ind.get_unit(sublayer, segment, unit)
