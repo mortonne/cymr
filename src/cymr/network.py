c75d6a1e2bba79921db595b743e46ea78903394b
Neal Morton <mortonne@gmail.com>
2020-08-04 02:02:54 -0500

    refactor: sublayers support for study

--- src/cymr/network.py
+++ src/cymr/network.py
@@ -558,7 +558,7 @@ class Network(object):
             s_slice = slice(*tuple(s_ind))
             mat[f_ind, s_slice] += self.c[s_slice] * L[i]
 
-    def study(self, segment, item_list, sublayer, B, Lfc, Lcf):
+    def study(self, segment, item_list, sublayers, B, Lfc, Lcf):
         """
         Study a list of items.
 
@@ -570,8 +570,8 @@ class Network(object):
         item_list : numpy.array
             Item indices relative to the segment.
 
-        sublayer : str
-            Sublayer of context to update.
+        sublayers : str
+            Sublayers of context to update.
 
         B : float or numpy.array
             Context updating rate. If an array, specifies a rate for
@@ -584,20 +584,24 @@ class Network(object):
         Lcf : float or numpy.array
             Learning rate for context to item associations.
         """
+        if not isinstance(sublayers, list):
+            sublayers = [sublayers]
+        n_item = item_list.shape[0]
+        n_sub = len(sublayers)
         if not isinstance(B, np.ndarray):
-            B = np.tile(B, item_list.shape).astype(float)
+            B = np.tile(B, (n_item, n_sub)).astype(float)
         if not isinstance(Lfc, np.ndarray):
-            Lfc = np.tile(Lfc, item_list.shape).astype(float)
+            Lfc = np.tile(Lfc, (n_item, n_sub)).astype(float)
         if not isinstance(Lcf, np.ndarray):
-            Lcf = np.tile(Lcf, item_list.shape).astype(float)
+            Lcf = np.tile(Lcf, (n_item, n_sub)).astype(float)
         f_ind = self.get_segment('f', *segment)
-        item_ind = f_ind[item_list]
-        c_ind = self.get_sublayer('c', sublayer)
+        item_ind = (f_ind[0] + item_list).astype(np.dtype('i'))
+        c_ind = self.get_sublayers('c', sublayers)
         operations.study(self.w_fc_exp, self.w_fc_pre,
                          self.w_cf_exp, self.c, self.c_in,
                          self.f, item_ind, c_ind, B, Lfc, Lcf)
 
-    def study_distract(self, segment, item_list, sublayer, B, Lfc, Lcf,
+    def study_distract(self, segment, item_list, sublayers, B, Lfc, Lcf,
                        distract_segment, distract_list, distract_B):
         """
         Study a list of items.
@@ -609,6 +613,9 @@ class Network(object):
 
         item_list : numpy.array
             Item indices relative to the segment.
+
+        sublayers : str or list of str
+            Sublayers to update during study.
 
         B : float or numpy.array
             Context updating rate. If an array, specifies a rate for
@@ -634,23 +641,25 @@ class Network(object):
             where distract_B[i] is zero.
         """
         f_ind = self.get_segment('f', *segment)
-        item_ind = f_ind[item_list]
+        item_ind = (f_ind[0] + item_list).astype(np.dtype('i'))
+        if not isinstance(sublayers, list):
+            sublayers = [sublayers]
+        n_item = item_list.shape[0]
+        n_sub = len(sublayers)
         if not isinstance(B, np.ndarray):
-            B = np.tile(B, item_list.shape).astype(float)
+            B = np.tile(B, (n_item, n_sub)).astype(float)
         if not isinstance(Lfc, np.ndarray):
-            Lfc = np.tile(Lfc, item_list.shape).astype(float)
+            Lfc = np.tile(Lfc, (n_item, n_sub)).astype(float)
         if not isinstance(Lcf, np.ndarray):
-            Lcf = np.tile(Lcf, item_list.shape).astype(float)
+            Lcf = np.tile(Lcf, (n_item, n_sub)).astype(float)
 
         f_ind = self.get_segment('f', *distract_segment)
-        distract_ind = f_ind[distract_list]
+        distract_ind = (f_ind[0] + distract_list).astype(np.dtype('i'))
 
         if not isinstance(distract_B, np.ndarray):
-            distract_B = np.tile(
-                distract_B, item_list.shape[0] + 1
-            ).astype(float)
+            distract_B = np.tile(distract_B, (n_item + 1, n_sub)).astype(float)
 
-        c_ind = self.get_sublayer('c', sublayer)
+        c_ind = self.get_sublayers('c', sublayers)
         operations.study_distract(
             self.w_fc_exp, self.w_fc_pre, self.w_cf_exp, self.c, self.c_in,
             self.f, item_ind, c_ind, B, Lfc, Lcf, distract_ind, distract_B
