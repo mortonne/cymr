73373563d2dfd61e747ec055c55d5c248d3d558e
Neal Morton <mortonne@gmail.com>
2020-08-12 00:18:34 -0500

    refactor: remove old p_recall and add sublayer support
    Remove python implementation of p_recall for simplicity of development

--- src/cymr/network.py
+++ src/cymr/network.py
@@ -715,64 +715,20 @@ class Network(object):
         state.append(self.copy())
         return state
 
-    def _p_recall_cython(self, segment, recalls, B, T, p_stop, amin=0.000001):
-        if not isinstance(B, np.ndarray):
-            B = np.tile(B, len(recalls)).astype(float)
-        rec_ind = self.f_ind[segment]
-        n_item = rec_ind.stop - rec_ind.start
-        exclude = np.zeros(n_item, dtype=np.dtype('i'))
-        p = np.zeros(len(recalls) + 1)
-        recalls = np.array(recalls, dtype=np.dtype('i'))
-        operations.p_recall(rec_ind.start, n_item, recalls,
-                            self.w_fc_exp, self.w_fc_pre,
-                            self.w_cf_exp, self.w_cf_pre,
-                            self.w_ff_exp, self.w_ff_pre,
-                            self.f, self.f_in, self.c, self.c_in,
-                            exclude, amin, B, T, p_stop, p)
-        return p
-
-    def _p_recall_python(self, segment, recalls, B, T, p_stop, amin=0.000001):
-        # weights to use for recall (assume fixed during recall)
-        rec_ind = self.f_ind[segment]
-        w_cf = self.w_cf_exp[rec_ind, :] + self.w_cf_pre[rec_ind, :]
-
-        exclude = np.zeros(w_cf.shape[0], dtype=bool)
-        p = np.zeros(len(recalls) + 1)
-        for output, recall in enumerate(recalls):
-            # project the current state of context; assume nonzero support
-            support = np.dot(w_cf, self.c)
-            support[support < amin] = amin
-
-            # scale based on choice parameter, set recalled items to zero
-            strength = np.exp((2 * support) / T)
-            strength[exclude] = 0
-
-            # probability of this recall, conditional on not stopping
-            p[output] = ((strength[recall] / np.sum(strength)) *
-                         (1 - p_stop[output]))
-
-            # remove recalled item from competition
-            exclude[recall] = True
-
-            # update context
-            self.present(segment, recall, B)
-
-        # probability of the stopping event
-        p[len(recalls)] = p_stop[len(recalls)]
-        return p
-
-    def p_recall(self, segment, recalls, B, T, p_stop, amin=0.000001,
-                 compiled=True):
+    def p_recall(self, segment, recalls, sublayers, B, T, p_stop, amin=0.000001):
         """
         Calculate the probability of a specific recall sequence.
 
         Parameters
         ----------
-        segment : str
-            Segment from which items are recalled.
+        segment : tuple of str, str
+            Sublayer and segment from which items are recalled.
 
         recalls : numpy.array
             Index of each recalled item relative to the segment.
+
+        sublayers : str or list of str
+            Sublayer(s) of context to update.
 
         B : float
             Context updating rate after each recalled item.
@@ -786,16 +742,28 @@ class Network(object):
         amin : float, optional
             Minimum activation for each not-yet-recalled item on each
             recall attempt.
-
-        compiled : bool, optional
-            If true, the compiled version of the function will be used.
         """
+        if not isinstance(sublayers, list):
+            sublayers = [sublayers]
+        recalls = np.array(recalls, dtype=np.dtype('i'))
+        n_item = recalls.shape[0]
+        n_sub = len(sublayers)
+        B = expand_param(B, (n_item, n_sub))
         if T < amin:
             T = amin
-        if compiled:
-            p = self._p_recall_cython(segment, recalls, B, T, p_stop, amin)
-        else:
-            p = self._p_recall_python(segment, recalls, B, T, p_stop, amin)
+        f_ind = self.get_segment('f', *segment)
+        start = f_ind[0]
+        n_f = f_ind[1] - f_ind[0]
+        c_ind = self.get_sublayers('c', sublayers)
+
+        exclude = np.zeros(n_f, dtype=np.dtype('i'))
+        p = np.zeros(len(recalls) + 1)
+        operations.p_recall(
+            start, n_f, recalls, self.w_fc_exp, self.w_fc_pre,
+            self.w_cf_exp, self.w_cf_pre, self.w_ff_exp, self.w_ff_pre,
+            self.f, self.f_in, self.c, self.c_in, c_ind, exclude,
+            amin, B, T, p_stop, p
+        )
         return p
 
     def generate_recall(self, segment, B, T, p_stop, amin=0.000001):
