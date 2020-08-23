3273560215ac198c073ad130616e3d68ab2e2c1b
Neal Morton <mortonne@gmail.com>
2020-08-23 18:00:37 -0500

    perf: update benchmark test for new api

--- benchmarks/likelihood.py
+++ benchmarks/likelihood.py
@@ -3,40 +3,48 @@
 import numpy as np
 
 from cymr import cmr
-from cymr import network
+from cymr import parameters
 
 
 class TimeLikelihood(object):
     def setup(self):
-        self.B = .8
-        self.L = 1
-        self.T = 10
-        self.X1 = .05
-        self.X2 = 1
         self.n_item = 24
         self.n_trial = 300
         self.n_item_total = 768
         self.n_dim = 300
-        self.patterns = np.random.normal(size=(self.n_item_total, self.n_dim))
+        mat = np.random.normal(size=(self.n_item_total, self.n_dim))
+        self.patterns = {'vector': {'loc': mat}}
         n_recall = np.random.choice(self.n_item, self.n_trial)
         items = np.arange(self.n_item_total)
-        self.study = [np.random.choice(items, self.n_item, replace=False)
+        item_index = [np.random.choice(items, self.n_item, replace=False)
                       for i in range(self.n_trial)]
-        inputs = np.arange(self.n_item)
-        self.recall = [np.random.choice(inputs, n, replace=False)
-                       for n in n_recall]
+        inputs = [np.arange(self.n_item) for i in range(self.n_trial)]
+        outputs = [np.random.choice(i, n, replace=False)
+                   for i, n in zip(inputs, n_recall)]
+
+        self.study = {'input': inputs, 'item_index': item_index}
+        self.recall = {'input': outputs}
+
+        param_def = parameters.Parameters()
+        param_def.set_dependent(Dfc='1 - Lfc', Dcf='1 - Lcf')
+        param_def.set_sublayers(f=['task'], c=['task'])
+        param_def.set_weights('fc', {
+            (('task', 'item'), ('task', 'item')): 'Dfc * loc',
+        })
+        param_def.set_weights('cf', {
+            (('task', 'item'), ('task', 'item')): 'Dcf * loc',
+        })
+        self.param_def = param_def
+
+        param = {
+            'B_enc': .5, 'B_start': 0, 'B_rec': .8,
+            'Lfc': .8, 'Lcf': .8, 'P1': 1, 'P2': 1,
+            'T': 10, 'X1': .05, 'X2': .2
+        }
+        self.param = param_def.eval_dependent(param)
+        self.model = cmr.CMRDistributed()
 
     def time_likelihood(self):
-        net = network.Network({'item': (self.n_item, self.n_dim),
-                               'start': (1, 1)})
-        p_stop = cmr.p_stop_op(self.n_item, self.X1, self.X2)
-        for study, recall in zip(self.study, self.recall):
-            net.reset()
-            item_patterns = self.patterns[study, :]
-            net.add_pre_weights('fc', ('item', 'item'), item_patterns)
-            net.add_pre_weights('cf', ('item', 'item'), item_patterns)
-            net.add_pre_weights('fc', ('start', 'start'), 1)
-            net.update('start', 0)
-            item_list = np.arange(len(study), dtype=int)
-            net.study('item', item_list, self.B, self.L, self.L)
-            p = net.p_recall('item', recall, self.B, self.T, p_stop)
+        self.model.likelihood_subject(
+            self.study, self.recall, self.param, self.param_def, self.patterns
+        )
