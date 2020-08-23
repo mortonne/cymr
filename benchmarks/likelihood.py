96eeea73ec4c5eef546ea5c6eceb1a94269264d3
Neal Morton <mortonne@gmail.com>
2020-08-23 18:27:51 -0500

    perf: use sample data in likelihood benchmark

--- benchmarks/likelihood.py
+++ benchmarks/likelihood.py
@@ -2,28 +2,17 @@
 
 import numpy as np
 
+from cymr import fit
 from cymr import cmr
 from cymr import parameters
 
 
 class TimeLikelihood(object):
     def setup(self):
-        self.n_item = 24
-        self.n_trial = 300
-        self.n_item_total = 768
-        self.n_dim = 300
-        mat = np.random.normal(size=(self.n_item_total, self.n_dim))
-        self.patterns = {'vector': {'loc': mat}}
-        n_recall = np.random.choice(self.n_item, self.n_trial)
-        items = np.arange(self.n_item_total)
-        item_index = [np.random.choice(items, self.n_item, replace=False)
-                      for i in range(self.n_trial)]
-        inputs = [np.arange(self.n_item) for i in range(self.n_trial)]
-        outputs = [np.random.choice(i, n, replace=False)
-                   for i, n in zip(inputs, n_recall)]
+        self.data = fit.sample_data('Morton2013_mixed')
 
-        self.study = {'input': inputs, 'item_index': item_index}
-        self.recall = {'input': outputs}
+        n_items = 768
+        self.patterns = {'vector': {'loc': np.eye(n_items)}}
 
         param_def = parameters.Parameters()
         param_def.set_dependent(Dfc='1 - Lfc', Dcf='1 - Lcf')
@@ -45,6 +34,6 @@ class TimeLikelihood(object):
         self.model = cmr.CMRDistributed()
 
     def time_likelihood(self):
-        self.model.likelihood_subject(
-            self.study, self.recall, self.param, self.param_def, self.patterns
+        self.model.likelihood(
+            self.data, self.param, None, self.param_def, self.patterns
         )
