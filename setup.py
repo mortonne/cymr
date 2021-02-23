732fa9ad4aff497bbb5cfbf5fc61e63861ecd35a
Neal Morton <mortonne@gmail.com>
2021-02-23 06:54:19 -0600

    build: bump patch version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='cymr',
-    version='0.10.2',
+    version='0.10.3',
     description='CMR implemented using Cython.',
     long_description=readme(),
     long_description_content_type="text/markdown",
