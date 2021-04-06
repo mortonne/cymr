9cdb3d8637013b8cc8a1b6c0a02dcd9ee4325079
Neal Morton <mortonne@gmail.com>
2021-04-06 02:02:39 -0500

    build: switch to using setup.cfg for most settings

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from Cython.Build import cythonize
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-setup(
-    name='cymr',
-    version='0.10.5',
-    description='CyMR: Computational modeling of free recall data.',
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    author='Neal Morton',
-    author_email='mortonne@gmail.com',
-    license='GPLv3',
-    url='http://github.com/mortonne/cymr',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    ext_modules=cythonize(['src/cymr/operations.pyx',
-                           'src/cymr/lba.pyx']),
-    extras_require={
-        'docs': ['sphinx', 'pydata-sphinx-theme', 'ipython'],
-        'test': ['pytest', 'codecov', 'pytest-cov'],
-    },
-    package_data={
-        'cymr': ['data/*.csv']
-    },
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.8',
-    ]
-)
+setup(ext_modules=cythonize(['src/cymr/operations.pyx', 'src/cymr/lba.pyx']))
